a9213a465384ddeeb6306207d7bfbdd9d6b1695b
MrPreet <harpreet9629@gmail.com>
2022-12-15 11:44:52 +0530

    Landing Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
-import Home from '@components/Home'
+import Head from 'next/head'
+import Image from 'next/image'
 
-export default Home
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Pinsta</title>
+        <meta name="description" content="Pinsta Web3 Pinterest on LensProtocol" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='w-full h-screen items-center justify-center flex flex-col'>
+        <Image src="/logo.png" alt="Pinsta Logo" width={400} height={400} />
+      </div>
+    </>
+  )
+}
