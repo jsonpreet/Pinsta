adeaf81a2bdcb857d2064f5506dd330a4d74bf1a
MrPreet <harpreet9629@gmail.com>
2022-12-15 11:23:46 +0530

    Landing Page

--- src/utils/functions/clearLocalStorage.ts
+++
@@ -1,6 +0,0 @@
-const clearLocalStorage = () => {
-  localStorage.removeItem('accessToken')
-  localStorage.removeItem('refreshToken')
-  localStorage.removeItem('pinsta.store')
-}
-export default clearLocalStorage
