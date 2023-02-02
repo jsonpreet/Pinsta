b219f8e9b817f4ce15f8d6c6356c18e4dac22a39
MrPreet <harpreet9629@gmail.com>
2023-02-02 20:54:51 +0530

    Adding new verifications on Pinsta Members

--- src/utils/data/pinsta-members.ts
+++ src/utils/data/pinsta-members.ts
@@ -2,4 +2,8 @@ export const pinstaMembers = [
     '0x016efc', // JsonPreet.lens
     '0x018ec4', // Pinsta.lens
     '0x010040', // Anku.lens
+    '0xacaf', // 0XZelda.lens
+    '0x01a2ee', // Wav3s.lens
+    '0x019b72', // Buttrfly.lens
+    '0xbbb9', // OrbApp.lens
 ]
